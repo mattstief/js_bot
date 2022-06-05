76a0543cc9ce1a3cbe56ae390486601929754d1f
Matt Stieferman <mstieferman@gmail.com>
2022-06-04 20:58:35 -0500

    added slash commands

--- index.ts
+++ index.ts
@@ -13,14 +13,48 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('ready!')
+
+    const guildId = '982793020736962581'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'replies pong',
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong'
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isCommand()) {
+        return
+    }
+
+    const {commandName, options} = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply ({
+            content: 'pong',
+            ephemeral: true,
         })
     }
 })
+
+// client.on('messageCreate', (message) => {
+//     if (message.content === 'ping') {
+//         message.reply({
+//             content: 'pong'
+//         })
+//     }
+//     if (message.content === '/play') {
+//         message.reply({
+//             content: 'pong'
+//         })
+//     }
+// })
 
 client.login(process.env.TOKEN)
